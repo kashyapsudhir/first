0b3605a875dbc0097998e4dea16b7fb3db7acbcd
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-03-05 09:44:08 +0530

    [Addtion]

---
+++ sudhir1/Addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudhir1
+{
+    public class Addition
+    {
+        public int add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
--- sudhir1/Program.cs
+++ sudhir1/Program.cs
@@ -1,10 +1,15 @@
-﻿public class Program
+﻿using sudhir1;
+
+public class Program
 {
     public static void Main()
     {
         int x = 2; int y = 3;
         int sum = x + y;
         Console.WriteLine(sum);
+        //object 
+        Addition plus = new Addition();
+      Console.WriteLine(plus.add(10,30));
     }
 
 }
